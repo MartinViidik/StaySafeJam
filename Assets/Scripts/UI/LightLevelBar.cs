adb25efdbb2b4962773ff4dac721747c01f215e3
Martin Viidik <martinviidik@gmail.com>
2020-03-29 19:36:14 +0300

    Light mechanics added

--- Assets/Scripts/UI/LightLevelBar.cs
+++ Assets/Scripts/UI/LightLevelBar.cs
@@ -1,20 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LightLevelBar : MonoBehaviour
 {
-    private Transform bar;
+    private Image bar;
 
     // Start is called before the first frame update
     private void Start()
     {
-        bar = transform.Find("Bar");
+        bar = transform.Find("Bar").GetComponent<Image>();
     }
 
     public void setSize(float sizeNormalized)
     {
-        bar.localScale = new Vector3(sizeNormalized, 1f);
+        bar.fillAmount = sizeNormalized;
     }
 
     // Update is called once per frame
