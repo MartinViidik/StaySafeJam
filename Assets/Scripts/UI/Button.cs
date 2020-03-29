2b7607cc0ab4eecb09c5cc65d4b5eef17497ef09
Davide Aldegheri <aldegheri.d@gmail.com>
2020-03-30 00:34:18 +0200

    added ui sounds

---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour, IPointerEnterHandler, ISelectHandler
+{
+    [SerializeField] private AudioSource rollOver;
+    [SerializeField] private AudioSource select;
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rollOver.Play();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        @select.Play();
+    }
+}
